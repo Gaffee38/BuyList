bd2481ef4e2f6b22f772f094e0c1771db8a44776
Gaffee38 <83159243+Gaffee38@users.noreply.github.com>
2023-09-18 14:22:20 +0800

    Updates

---
+++ js/about.e0b2c0ae.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkbuy_list"]=self["webpackChunkbuy_list"]||[]).push([[443],{2472:function(n,r,t){t.r(r),t.d(r,{default:function(){return o}});var u=t(3396);const e={class:"favoriter"},s=(0,u._)("h1",null,"Страница с избранными товарами",-1),c=[s];function i(n,r){return(0,u.wg)(),(0,u.iD)("div",e,c)}var a=t(89);const l={},f=(0,a.Z)(l,[["render",i]]);var o=f}}]);
+//# sourceMappingURL=about.e0b2c0ae.js.map
